6b3487844a108e68d9cf2e1061da0805cbeb4729
KIMBERLY LENSICO <KIMBERLYLENSICO@OUTLOOK.COM>
2020-10-14 15:46:01 -0700

    part 2 of lab 6

--- server.js
+++ server.js
@@ -15,10 +15,33 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 
+app.get('/location', handleLocation);
+
+function handleLocation(request, response) {
+  let locationData = require('./data/location.json');
+  const city = request.query.city;
+  response.json(new Location(city, locationData[0]));
+}
+
+function Location(city, locationData) {
+  this.search_query = city;
+  this.formatted_query = locationData.display_name;
+  this.latitude = locationData.lat;
+  this.longitude = locationData.lon;
+}
+
 app.get('/', (request, response) => {
-  response.send('Hello World~!');
+  response.send('Hello World!');
 });
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 });
+
+// FOR REFERENCE:
+// {
+//   "search_query": "seattle",
+//   "formatted_query": "Seattle, WA, USA",
+//   "latitude": "47.606210",
+//   "longitude": "-122.332071"
+// }
