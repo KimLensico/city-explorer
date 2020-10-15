f2b961f55a8dab6dd23ed0aeb2489d4c863546dd
KIMBERLY LENSICO <KIMBERLYLENSICO@OUTLOOK.COM>
2020-10-15 13:58:34 -0700

    submitting things for lab 7

--- server.js
+++ server.js
@@ -3,24 +3,29 @@
 // allowing us to user the dotenv file to declare environment variables.
 // example: the PORT variable
 require('dotenv').config();
-
 const express = require('express');
-
 const app = express();
-
 const cors = require('cors');
+const superagent = require('superagent');
 
 // we are getting the port from the env file
 const PORT = process.env.PORT;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 
 app.use(cors());
 
 app.get('/location', handleLocation);
 
 function handleLocation(request, response) {
-  let locationData = require('./data/location.json');
   const city = request.query.city;
-  response.json(new Location(city, locationData[0]));
+  // getting the city variable
+  const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
+  superagent.get(url)
+    .then(data => {
+      // we will declare a new variable to use the API request through super agent instead of the json file 
+      const locationData = data.body[0];
+      response.json(new Location(city, locationData));
+    });
 }
 
 function Location(city, locationData) {
@@ -44,9 +49,8 @@ function handleWeather(request, response) {
   // declare a variable named weather data that has its contents as the entire json file 
   let weatherData = require('./data/weather.json');
   // declaring a new array called days
-  const days = [];
   // for each day in weatherData, we will create a new Object
-  weatherData.data.forEach(day => days.push(new Day(day)));
+  const days = weatherData.data.map(day => new Day(day));
   response.send(days);
 }
 
@@ -54,16 +58,3 @@ function Day(dayData) {
   this.forecast = dayData.weather.description;
   this.time = dayData.datetime;
 }
-
-// FOR REFERENCE:
-// [
-//   {
-//     "forecast": "Partly cloudy until afternoon.",
-//     "time": "Mon Jan 01 2001"
-//   },
-//   {
-//     "forecast": "Mostly cloudy in the morning.",
-//     "time": "Tue Jan 02 2001"
-//   },
-//   ...
-// ]
